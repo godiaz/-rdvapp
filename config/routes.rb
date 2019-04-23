9247776d75ca14c887ebbc87d61877f8a34f9913
Gonzalo Diaz <info@ventra.com.co>
2019-04-23 17:09:40 +0100

    rendezvous update april 23rd

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'unclassifiable', to: 'pages#unclassifiable', as: :unclassifiable
   get 'wouldyou', to: 'pages#wouldyou', as: :wouldyou
   get 'contact', to: 'pages#contact', as: :contact
+  get '/hotels', to: 'pages#hotels'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
